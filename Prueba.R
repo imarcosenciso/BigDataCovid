04f1dfa9b5ec69ac16ccadac428e72fb5644e350
unknown <mikelfuentes@opendeusto.es>
2020-12-03 21:43:50 +0100

    nope

--- Prueba.R
+++ Prueba.R
@@ -7,7 +7,7 @@ cat
 
 
 # Librerías
-
+library(tidyverse)
 
 # Datos generales
 datos_generales = read.csv("Datos/Generales.csv",stringsAsFactors = FALSE, sep=';')
@@ -21,5 +21,15 @@ datos_generales = subset( datos_generales,
                           )
 # Cambiamos la columna fecha de formato String a Date.
 datos_generales$Fecha = as.Date(datos_generales$Fecha, format = "%d/%m/%Y")
+datos_generales$PCR_diarios = NA
+datos_generales[1,5] = datos_generales[1, 2]
+for (i in 1:nrow(datos_generales)) { 
+  prim = datos_generales[i, 2]
+  sec = datos_generales[i+1, 2]
+  res = sec - prim
+  
+  datos_generales[(i+1),5] = res
+  
+}
 
 summary(datos_generales)
